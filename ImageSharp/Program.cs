ec255c9822211d92c9b1931a5beb83f948769cd8
Dmitriy Sereda <dmitiriy.sereda@outlook.com>
2019-02-28 23:30:41 +0200

    Solution files

---
+++ ImageSharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Processing;
+
+namespace ImageSharp
+{
+    static class Program
+    {
+        private const string Filename = "w2.jpg";
+
+        private static void Main()
+        {
+            var path = "../../../" + Filename;
+            Stream fileStream = File.OpenRead(path);
+            for (var i = 0; i < 50; i++)
+            {
+                fileStream.Position = 0;
+                var watch = Stopwatch.StartNew();
+                using (var image = Image.Load(fileStream))
+                {
+                    using (var outStream = new MemoryStream())
+                    {
+                        image.Mutate(x => x.BlackWhite());
+                        image.Save(outStream, new PngEncoder());
+                    }
+                }
+
+                watch.Stop();
+                Console.WriteLine($"Operation {i + 1}: {watch.ElapsedMilliseconds}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SkiaSharp/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkiaSharp
+{
+    static class Program
+    {
+        private const string Filename = "w2.jpg";
+
+        private static void Main()
+        {
+            var path = "../../../" + Filename;
+            Stream fileStream = File.OpenRead(path); // open a stream to an image file
+
+            for (int i = 0; i < 25; i++)
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                fileStream.Position = 0;
+
+                // decode the bitmap from the stream
+                using (var stream = new SKManagedStream(fileStream))
+                using (var bitmap = SKBitmap.Decode(stream))
+
+                using (var paint = new SKPaint())
+                {
+                    //Black & White color matrix
+                    paint.ColorFilter = paint.ColorFilter =
+                        SKColorFilter.CreateColorMatrix(new float[]
+                        {
+                                1.5f, 1.5f, 1.5f, 0, -1.5f,
+                                1.5f, 1.5f, 1.5f, 0, -1.5f,
+                                1.5f, 1.5f, 1.5f, 0, -1.5f,
+                                0,    0,    0,    1,  0
+                        }); ;
+
+                    Console.WriteLine($"Apply color filter {watch.ElapsedMilliseconds}");
+
+                    using (var toBitmap = new SKBitmap(bitmap.Width, bitmap.Height, bitmap.ColorType, bitmap.AlphaType))
+                    using (var canvas = new SKCanvas(toBitmap))
+                    {
+                        Console.WriteLine($"New canvas {watch.ElapsedMilliseconds}");
+
+                        // draw the bitmap through the filter
+                        canvas.DrawBitmap(bitmap, SKRect.Create(bitmap.Width, bitmap.Height), paint);
+                        Console.WriteLine($"DrawBitmap {watch.ElapsedMilliseconds}");
+
+                        canvas.Flush();
+
+                        Console.WriteLine($"Flush {watch.ElapsedMilliseconds}");
+
+
+                        using (SKImage img = SKImage.FromBitmap(toBitmap))
+                        {
+                            Console.WriteLine($"Encode before {watch.ElapsedMilliseconds}");
+                            using (SKData data = img.Encode())
+                            {
+                                Console.WriteLine($"Encode after {watch.ElapsedMilliseconds}");
+                                using (Stream outputStream = new MemoryStream())
+                                {
+                                    data.AsStream().CopyTo(outputStream);
+                                    Console.WriteLine($"Save to MemoryStream {watch.ElapsedMilliseconds}");
+                                }
+
+                            }
+                        }
+                    }
+                }
+
+                watch.Stop();
+                Console.WriteLine($"Total: {watch.ElapsedMilliseconds}");
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
